e5edd499707b9c821c6e92686195cc3549340b3d
Silentor666 <silentor@users.noreply.github.com>
2024-07-25 18:34:07 +0300

    WIP Add UIToolkit realization

---
+++ Assets/Scripts/TreeList/Editor/TreeListPropertyDrawer.UIToolkit.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Silentor.TreeList.Editor
+{
+    /// <summary>
+    /// UIToolkit version of TreeListPropertyDrawer
+    /// </summary>
+    public partial class TreeListPropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI( SerializedProperty property )
+        {
+            var root = new VisualElement() { style = { flexDirection = FlexDirection.Row } };
+            var foldout = new Foldout( )
+                          {
+                                  text  = property.displayName,
+                                  value = property.isExpanded,
+                                  //bindingPath = property.propertyPath,
+                          };
+
+            foldout.BindProperty( property );
+            root.Add( foldout );
+
+            var elementsCounter = new TextField(){ style = { position = Position.Absolute, right = 0, top = 0, width = 50 } };
+            elementsCounter.SetEnabled( false );
+            elementsCounter.BindProperty( property.FindPropertyRelative( "SerializableNodes" ).FindPropertyRelative( "Array.size" ) );
+            root.Add( elementsCounter );
+
+            var removeButton = new Button( () =>
+                            {
+                                //AddItem(  );
+                            } )
+                            { text = "-", style = { position = Position.Absolute, right = 55, top = 0, width = EditorGUIUtility.singleLineHeight, height = EditorGUIUtility.singleLineHeight } };
+            root.Add( removeButton );
+
+            var addButton = new Button( () =>
+                                        {
+                                            //AddItem(  );
+                                        } )
+                            { text = "+", style = { position = Position.Absolute, right = 50 + 5 + EditorGUIUtility.singleLineHeight + 10, top = 0, width = EditorGUIUtility.singleLineHeight, height = EditorGUIUtility.singleLineHeight } };
+            root.Add( addButton );
+
+            var tree = new TreeView();
+            tree.makeItem = ( ) => new InspectorElement();
+            tree.bindItem = ( e, i ) =>
+            {
+                var nodes = property.FindPropertyRelative( "SerializableNodes" );
+                var nodeProp = nodes.GetArrayElementAtIndex( i );
+                var valueProp = nodeProp.FindPropertyRelative( "Value" );
+                var levelProp = nodeProp.FindPropertyRelative( "Depth" );
+
+                if ( valueProp.hasVisibleChildren )
+                {
+                    var enterChildren = true;
+                    var endProp       = valueProp.GetEndProperty();
+                    while ( valueProp.NextVisible( enterChildren ) && !SerializedProperty.EqualContents( valueProp, endProp ) )
+                    {
+                        var label          =  String.Concat( Enumerable.Repeat( "    ", levelProp.intValue )) + valueProp.displayName;
+                        var propField      = new PropertyField( valueProp, label );
+                        e.Add( propField );
+                        enterChildren   =  false;
+                    }
+                }
+                else   //Value is primitive type itself
+                {
+                    var label     =  String.Concat( Enumerable.Repeat( "    ", levelProp.intValue )) + valueProp.displayName;
+                    var propField = new PropertyField( valueProp, label );
+                    e.Add( propField );
+                }
+            };
+            var nodes = property.FindPropertyRelative( "SerializableNodes" );
+            foreach ( var VARIABLE in nodes )
+            {
+                
+            }
+            tree.SetRootItems(  );
+            foldout.contentContainer.Add( tree );
+
+            return root;
+        }
+
+        private IList<TreeViewItemData<SerializedProperty>> GetHierarchy( SerializedProperty nodesProp )
+        {
+            var nodes = new List<TreeViewItemData<SerializedProperty>>();
+            for ( var i = 0; i < nodesProp.arraySize; i++ )
+            {
+                var nodeProp = nodesProp.GetArrayElementAtIndex( i );
+                var depthProp = nodeProp.FindPropertyRelative( "Depth" );
+                nodes.Add( new TreeViewItemData<SerializedProperty>( i, nodeProp, depthProp.intValue ) );
+            }
+
+            return nodes;
+        }
+    }
+
+    
+
+}
--- Assets/Scripts/TreeList/Editor/TreeListPropertyDrawer.cs
+++ Assets/Scripts/TreeList/Editor/TreeListPropertyDrawer.cs
@@ -1,204 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using UnityEditor;
-using UnityEditor.IMGUI.Controls;
-using UnityEngine;
 using Object = System.Object;
 
 namespace Silentor.TreeList.Editor
 {
+    /// <summary>
+    ///     Common part of TreeListPropertyDrawer
+    /// </summary>
     [CustomPropertyDrawer( typeof(TreeList<>), true )]
-    public class TreeListPropertyDrawer : PropertyDrawer
-    {                                   
-        private static readonly Dictionary<Int32, TreeEditorState> _treeViewStates = new ();
-        private                 MyTreeView                         _tree;
-        private                 MultiColumnHeaderState             _multiColumnHeaderState;
-        private readonly        Single                             _headerHeight = EditorGUIUtility.singleLineHeight + 2;
+    public partial class TreeListPropertyDrawer : PropertyDrawer
+    {
         private                 Int32                              _structuralHash;
         private                 String                             _treeTypeHint;
-
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label )
-        {
-            //Debug.Log( $"{property.displayName}, evt {Event.current.type}, hash {this.GetHashCode()}" );
-
-            var state     = GetPersistentTreeViewState( property );
-            var nodesProp = property.FindPropertyRelative( "SerializableNodes" );
-            if( nodesProp.arraySize == 0 )
-                property.isExpanded = false;
-
-            if ( _tree == null )
-            {
-                _multiColumnHeaderState = new MultiColumnHeaderState( 
-                        new MultiColumnHeaderState.Column[]
-                        {
-                                new ()
-                                {
-                                        autoResize            = false,
-                                },
-                                new ()
-                                {
-                                        autoResize            = false,
-                                },
-                        
-                        } );
-
-                _tree = new MyTreeView( state.TreeViewState, new MyMultiColumnHeader(_multiColumnHeaderState), nodesProp ) ;
-            }
-
-            position = DrawHeader( position, label, state, nodesProp, property );
-
-            if ( !property.isExpanded )
-            {
-                return;
-            }
-
-            if ( !_tree.IsInitialized || _structuralHash != GetStructuralHash( nodesProp ) )
-            {
-                _tree.Reload();
-                _structuralHash = GetStructuralHash( nodesProp );
-            }
-
-            //Adjust columns
-            var expandedItems = _tree.GetRows().Where( tvi => _tree.GetExpanded().Contains( tvi.id ) ).ToArray();
-            if ( expandedItems.Length > 0 )
-            {
-                //Expand foldouts column to fit the deepest expanded item
-                var deepestExpandedItem = expandedItems.MaxBy( tvi => tvi.depth );
-                var depth               = deepestExpandedItem.depth * 15 + 30;
-                _multiColumnHeaderState.columns[0].width = depth;
-            }
-            else
-            {
-                _multiColumnHeaderState.columns[0].width           = 30;
-            }
-                    
-            _multiColumnHeaderState.columns[1].width = position.width - _multiColumnHeaderState.columns[0].width - 20;  
-
-            _tree.OnGUI( position );
-        }
-
-        private Rect DrawHeader( Rect totalRect, GUIContent label, TreeEditorState state, SerializedProperty nodesProp, SerializedProperty mainProperty )
-        {
-            var headerRect  = new Rect( totalRect.x, totalRect.y,                totalRect.width, _headerHeight );
-            var contentRect = new Rect( totalRect.x, totalRect.y + _headerHeight, totalRect.width, totalRect.height - _headerHeight );
-
-            //Draw header hint
-            GUI.Label( headerRect, GetTreeHint( nodesProp ), EditorStyles.centeredGreyMiniLabel );
-
-            //Draw items count
-            var itemsCountRect = new Rect( headerRect.x + headerRect.width - 50, headerRect.y, 50, _headerHeight );
-            GUI.enabled = false;
-            GUI.TextField( itemsCountRect, nodesProp.arraySize.ToString() );
-            GUI.enabled = true;
-
-            //Draw remove button
-            var btnRect      = new Rect( itemsCountRect.x - _headerHeight - 5, headerRect.y, _headerHeight, _headerHeight );
-            var isBtnEnabled = mainProperty.isExpanded && _tree.HasSelection();
-            EditorGUI.BeginDisabledGroup( !isBtnEnabled );
-            if ( GUI.Button( btnRect, Resources.Minus, Resources.ToolBarBtnStyle ) )
-            {
-                var (_, selectedIndex) = _tree.GetSelectedItem();
-                RemoveItem( selectedIndex, nodesProp );
-                if( nodesProp.arraySize == 0 )
-                    mainProperty.isExpanded = false;
-            }
-            EditorGUI.EndDisabledGroup();
-
-            //Draw add button
-            btnRect = new Rect( btnRect.x - btnRect.width - 15, btnRect.y, btnRect.width, btnRect.height );
-            var isEmptyTree = nodesProp.arraySize == 0;
-            isBtnEnabled = isEmptyTree || _tree.HasSelection();
-            EditorGUI.BeginDisabledGroup( !isBtnEnabled );
-            if ( GUI.Button( btnRect, Resources.Plus, Resources.ToolBarBtnStyle  ) )
-            {
-                if ( isEmptyTree )
-                {
-                    AddItem( -1,  nodesProp );
-                    mainProperty.isExpanded = true;
-                }
-                else
-                {
-                    var (_, selectedIndex) = _tree.GetSelectedItem();
-                    AddItem( selectedIndex, nodesProp );
-                }
-            };
-            EditorGUI.EndDisabledGroup();
-
-            //Custom prefix label because TreeView with header hides it. wtf?
-            var prefixLabelRect = headerRect;
-            prefixLabelRect.xMax = btnRect.x - 5;
-            var headerStyle = nodesProp.prefabOverride ? Resources.HeaderOverridenStyle : Resources.HeaderStyle;
-            if( nodesProp.arraySize > 0 )
-                mainProperty.isExpanded = EditorGUI.Foldout( prefixLabelRect, mainProperty.isExpanded, label, true, headerStyle );   
-            else
-            {
-                EditorGUI.Foldout( prefixLabelRect, false, label, true, headerStyle );
-                mainProperty.isExpanded = false;
-            }
-
-            //Draw blue margin if tree value is overriden
-            if ( nodesProp.prefabOverride && Event.current.type == EventType.Repaint )
-            {
-                EditorGUI.DrawRect( new Rect(headerRect.x - 18, headerRect.y, 2, headerRect.height), Resources.PrefabOverrideMarginColor);
-            }
-
-            //Simulate default property context menu
-            if (Event.current.type == EventType.MouseUp && prefixLabelRect.Contains (Event.current.mousePosition) && Event.current.button == 1)
-            {
-                var menu = new GenericMenu();
-                menu.AddItem( new GUIContent( "Copy property path" ), false, () => EditorGUIUtility.systemCopyBuffer = mainProperty.propertyPath );
-                if ( nodesProp.prefabOverride )
-                {
-                    //Find prefabs where this property may be overriden
-                    var  obj     = ((Component)nodesProp.serializedObject.targetObject).gameObject;
-                    var  prefab  = PrefabUtility.GetCorrespondingObjectFromSource( obj );  //Make sure we in prefab space
-
-                    List<GameObject> prefabAssets = new();
-                    do
-                    {
-                        var originalPrefabAsset = PrefabUtility.GetCorrespondingObjectFromOriginalSource( prefab );
-                        if( originalPrefabAsset )
-                            prefabAssets.Add( originalPrefabAsset );
-                        prefab = prefab.transform.parent?.gameObject;
-                    } while ( prefab );
-
-                    prefabAssets.Reverse();
-                    foreach ( var prefabAsset in prefabAssets )
-                    {
-                        var title = prefabAsset == prefabAssets.Last() ? $"Apply to Prefab '{prefabAsset.name}'" : $"Apply as Override in Prefab '{prefabAsset.name}'";
-                        if( prefabAsset == prefabAssets.Last() )
-                            menu.AddItem( new GUIContent( title ), false, 
-                                    () => PrefabUtility.ApplyPropertyOverride( nodesProp, AssetDatabase.GetAssetPath( prefabAsset ), InteractionMode.UserAction ) );
-                        else
-                            menu.AddItem( new GUIContent( title ), false, 
-                                    () => PrefabUtility.ApplyPropertyOverride( nodesProp, AssetDatabase.GetAssetPath( prefabAsset ), InteractionMode.UserAction ) );
-                    }
-
-                    menu.AddItem( new GUIContent( "Revert" ), false, ( ) => nodesProp.prefabOverride = false );
-                }
-                menu.AddSeparator( String.Empty );
-
-                if( !mainProperty.hasMultipleDifferentValues )
-                    menu.AddItem( new GUIContent( "Copy" ), false, () => Resources.Clipboard.Copy( mainProperty ) );
-                else
-                    menu.AddDisabledItem( new GUIContent( "Copy" ) );
-                if( !mainProperty.hasMultipleDifferentValues && GUI.enabled && Resources.Clipboard.IsPropertyPresent() )
-                    menu.AddItem( new GUIContent( "Paste" ), false, () =>
-                    {
-                        Resources.Clipboard.Paste( mainProperty );
-                        mainProperty.serializedObject.ApplyModifiedProperties();
-                    } );
-                else
-                    menu.AddDisabledItem( new GUIContent( "Paste" ) );
-
-                menu.ShowAsContext();
-                Event.current.Use ();
-            }
-
-            return contentRect;
-        }
 
         private void AddItem( Int32 parentIndex, SerializedProperty nodes )
         {
@@ -208,20 +22,20 @@ namespace Silentor.TreeList.Editor
                 var childIndex  = nodes.arraySize;
 
                 for ( var i = parentIndex + 1; i < nodes.arraySize; i++ )
-                {
                     if ( nodes.GetArrayElementAtIndex( i ).FindPropertyRelative( "Depth" ).intValue <= parentDepth )
                     {
                         childIndex = i;
                         break;
                     }
-                }
-                
+
                 nodes.InsertArrayElementAtIndex( childIndex );
                 var newItem = nodes.GetArrayElementAtIndex( childIndex  );
                 newItem.FindPropertyRelative( "Depth" ).intValue = parentDepth + 1;
             }
             else
+            {
                 nodes.InsertArrayElementAtIndex( 0 );
+            }
         }
 
         private void RemoveItem( Int32 itemIndex, SerializedProperty nodes )
@@ -230,41 +44,17 @@ namespace Silentor.TreeList.Editor
             var depth = nodes.GetArrayElementAtIndex( itemIndex ).FindPropertyRelative( "Depth" ).intValue;
             nodes.DeleteArrayElementAtIndex( itemIndex );
             while ( nodes.arraySize > itemIndex )
-            {
-                if( nodes.GetArrayElementAtIndex( itemIndex ).FindPropertyRelative( "Depth" ).intValue > depth )
+                if ( nodes.GetArrayElementAtIndex( itemIndex ).FindPropertyRelative( "Depth" ).intValue > depth )
                     nodes.DeleteArrayElementAtIndex( itemIndex );
                 else
                     break;
-            }
-        }
-
-        public override Single GetPropertyHeight( SerializedProperty property, GUIContent label )
-        {
-            if( _tree == null || !_tree.IsInitialized  )
-                return _headerHeight;
-            if( !property.isExpanded || property.FindPropertyRelative( "SerializableNodes" ).arraySize == 0)
-                return _headerHeight;
-
-            return Mathf.Clamp( _tree.totalHeight + _headerHeight, _headerHeight, 500 );
-        }
-
-        private TreeEditorState GetPersistentTreeViewState( SerializedProperty prop )
-        {
-            var key = HashCode.Combine( prop.serializedObject.targetObject.GetInstanceID(), prop.propertyPath.GetHashCode() );
-            if ( !_treeViewStates.TryGetValue( key, out var result ) )
-            {
-                result = new TreeEditorState();
-                _treeViewStates.Add( key, result );
-            }
-
-            return result;
         }
 
         private Int32 GetStructuralHash( SerializedProperty nodesProp )
         {
             var hash = new HashCode();
 
-            for ( int i = 0; i < nodesProp.arraySize; i++ )
+            for ( var i = 0; i < nodesProp.arraySize; i++ )
             {
                 var depthProp = nodesProp.GetArrayElementAtIndex( i ).FindPropertyRelative( "Depth" );
                 hash.Add( depthProp.intValue );
@@ -276,83 +66,54 @@ namespace Silentor.TreeList.Editor
         private String GetTreeHint( SerializedProperty nodesProp )
         {
             if ( nodesProp.arraySize == 0 )
+            {
                 return "Press + to add root item";
-            else
-            {
-                if ( _treeTypeHint == null )
-                {
-                    var node      = nodesProp.GetArrayElementAtIndex( 0 );
-                    var valueProp = node.FindPropertyRelative( "Value" );
-                    if( valueProp == null )
-                        _treeTypeHint = "Value is not serializable";
-                    else
-                    {
-                        var valueType = valueProp.type;
-                        _treeTypeHint =  $"Tree of {valueType}";
-                    }
-                }
-
-                return _treeTypeHint;
             }
-        }
-       
 
-        [Serializable]
-        private class TreeEditorState
-        {
-            public TreeViewState TreeViewState = new();
-            //public Boolean       IsTreeExpanded;
-        }
-
-        private static class Resources
-        {
-            public static readonly GUIStyle HeaderStyle = new (EditorStyles.foldoutHeader)
-                                                          {
-                                                                  fontStyle = FontStyle.Normal,
-                                                          };
-            public static readonly GUIStyle HeaderOverridenStyle = new (EditorStyles.foldoutHeader)
-                                                          {
-                                                                  fontStyle = FontStyle.Bold,
-                                                          };
-            public static readonly GUIStyle ToolBarBtnStyle = new (GUI.skin.button) {       
-                                                                                            alignment = TextAnchor.MiddleCenter,
-                                                                                            fontSize = 16,
-                                                                                            padding = new RectOffset()
-                                                                                    };
-
-            public static readonly GUIContent Plus  = new  (EditorGUIUtility.IconContent("Toolbar Plus").image, "Add child node") ;
-            public static readonly GUIContent Minus = new  (EditorGUIUtility.IconContent("Toolbar Minus").image, "Remove node") ;
-            public static readonly GUIContent Depth =  EditorGUIUtility.isProSkin 
-                    ? new ("Depth", EditorGUIUtility.IconContent("d_BlendTree Icon").image) 
-                    : new ("Depth", EditorGUIUtility.IconContent("BlendTree Icon").image) ;
-
-            
-
-            public static readonly Color PrefabOverrideMarginColor = new (0.003921569f, 0.6f, 0.92156863f, 0.75f);
-
-            public static class Clipboard
+            if ( _treeTypeHint == null )
             {
-                private static readonly Type       ClipboardType = typeof(ClipboardUtility).Assembly.GetType("UnityEditor.Clipboard");
-                private static readonly MethodInfo SetSerializedPropertyMethod   = ClipboardType.GetMethod("SetSerializedProperty", BindingFlags.Static | BindingFlags.Public);
-                private static readonly MethodInfo GetSerializedPropertyMethod    = ClipboardType.GetMethod("GetSerializedProperty", BindingFlags.Static | BindingFlags.Public);
-                private static readonly MethodInfo CheckMethod   = ClipboardType.GetMethod("HasSerializedProperty", BindingFlags.Static | BindingFlags.Public);
-
-                public static void Copy( SerializedProperty property )
+                var node      = nodesProp.GetArrayElementAtIndex( 0 );
+                var valueProp = node.FindPropertyRelative( "Value" );
+                if ( valueProp == null )
                 {
-                    SetSerializedPropertyMethod.Invoke( null, new Object[] { property } );
+                    _treeTypeHint = "Value is not serializable";
                 }
-
-                public static void Paste( SerializedProperty property )
+                else
                 {
-                    GetSerializedPropertyMethod.Invoke( null, new Object[] { property } );
-                }
-
-                public static Boolean IsPropertyPresent( )
-                {
-                    return (Boolean)CheckMethod.Invoke( null, Array.Empty<Object>() );
+                    var valueType = valueProp.type;
+                    _treeTypeHint =  $"Tree of {valueType}";
                 }
             }
 
+            return _treeTypeHint;
+        }
+
+        public static class Clipboard
+        {
+            private static readonly Type       ClipboardType = typeof(ClipboardUtility).Assembly.GetType( "UnityEditor.Clipboard" );
+
+            private static readonly MethodInfo SetSerializedPropertyMethod   =
+                    ClipboardType.GetMethod( "SetSerializedProperty", BindingFlags.Static | BindingFlags.Public );
+
+            private static readonly MethodInfo GetSerializedPropertyMethod    =
+                    ClipboardType.GetMethod( "GetSerializedProperty", BindingFlags.Static | BindingFlags.Public );
+
+            private static readonly MethodInfo CheckMethod   = ClipboardType.GetMethod( "HasSerializedProperty", BindingFlags.Static | BindingFlags.Public );
+
+            public static void Copy( SerializedProperty property )
+            {
+                SetSerializedPropertyMethod.Invoke( null, new Object[] { property } );
+            }
+
+            public static void Paste( SerializedProperty property )
+            {
+                GetSerializedPropertyMethod.Invoke( null, new Object[] { property } );
+            }
+
+            public static Boolean IsPropertyPresent( )
+            {
+                return (Boolean)CheckMethod.Invoke( null, Array.Empty<Object>() );
+            }
         }
     }
 }
